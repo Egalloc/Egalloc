d9492b3a696c053339f69820edbd3733aced3664
zifanshi <zifanshi@usc.edu>
2018-02-18 03:05:47 -0800

    API communicator

---
+++ APIResponse.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class APIResponse {
+    SearchInfo searchInformation;
+    Item[] items;
+
+    public SearchInfo getSearchInformation() {
+        return searchInformation;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+}
